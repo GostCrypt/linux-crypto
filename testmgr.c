08bff40a03aeda17416511bee6f7fb331ceca117
Dmitry Baryshkov <dbaryshkov@gmail.com>
2022-02-13 01:16:32 +0300

    testmgr: support newer kernels
    Since the commit 0eb76ba29d16 ("crypto: remove cipher routines from
    public crypto API") cipher routines (used by the test manager) were
    pushed into separate header file (and separate namespace). Add
    corresponding changes.
    
    Signed-off-by: Dmitry Baryshkov <dbaryshkov@gmail.com>

--- testmgr.c
+++ testmgr.c
@@ -29,10 +29,10 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/version.h>
-#if 0
-#include <crypto/aead.h>
-#include <crypto/akcipher.h>
-#include <crypto/kpp.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+#include <crypto/internal/cipher.h>
+MODULE_IMPORT_NS(CRYPTO_INTERNAL);
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 1, 0)
